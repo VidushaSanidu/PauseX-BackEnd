071a26559dd08f57deea08aa58129f51c6d23e61
VidushaSanidu <vidushsanidu07@gmail.com>
2024-04-08 15:05:26 +0530

    db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [SongsModule],
+  imports: [
+    SongsModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: process.env.DB_PASSWORD,
+      database: 'pauseXDB',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
